a6f8d2bdeedd51399934449676c72a146ab2c23b
Gus Salese <gustavo.salese@gmail.com>
2024-01-26 19:04:17 -0300

    ingresomateriales2601

--- js/index.js
+++ js/index.js
@@ -1,13 +1,84 @@
 
-/* Proyecto de App para Stock e inventario/*
+// Proyecto de App para Stock e inventario
+
+// alert ("Bienvenido! Por favor seleccione la opcion de logueo:")
 
 
-// Nuevo Usuario
+// let nuvoUsuario = "true"
+
+// const LEGAJOUSUARIO = prompt ("Ingrese su Legajo:") 
+// let passwordUsuario = prompt("Ingrese su Password:")
+
+
+// if (LEGAJOUSUARIO != ""){
+//     console.log("Ingresando al sistema: " + LEGAJOUSUARIO);
+// }
+// else {
+//     alert("Error! Por favor solicite Usuario del Sistema");
+// }
+
+
+// Buscar Material
+
+let nombreMaterial = prompt ("Ingrese el nombre del material");
+const PARTCODE = "1234"
+
+if ((nombreMaterial == "refrigerador") || (nombreMaterial == "heladera"))
+    console.log (nombreMaterial + "el codigo de parte buscado es" + "" + PARTCODE);
+    else {
+console.log ("no existe el material")
+    }
 
 
 
 
+
+// Ingreso de materiales:
+
+// let materialLavarropas = 0;
+
+// for (let i = 1; i == 10; i++){
+
+//     if (materialLavarropas == 5){
+//         console.log ("Se uso el 50% del stock, pedir Compra")
+//     }
+// }
+
+
+
+// let continuarOno = "si"
+
+// if (continuarOno != true){ // Nuevo Usuario
+
+// let nombre = prompt ("Nombre de Usuario:")
+// let apellido = prompt ("Apellido de Usuario:")
+// const legajo = prompt ("Ingrese su legajo:")
+
+// console.log (nombre + " " + apellido + " " + legajo)
+// } else {
+//     alert ("salir del sistema")
+// }
 
 // Login de Usuario
 
-*/
+
+
+// Ingreso de entrada de materiales
+
+// let materialLavarropas= 0
+// let materialHorno=0
+// let materialTostadora=0
+
+// let ingreso = prompt ("Ingrese el material")
+
+//     switch (ingreso){
+
+//         case "Lavarropas": materialLavarropas++;
+
+//         case "Horno": materialHorno++;
+
+//         case "Tostadora": materialTostadora++;
+
+//     }
+
+//     console.log ("Hay" + materialLavarropas)
