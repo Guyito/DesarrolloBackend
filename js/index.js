363fb3f4c77a2c873bbedba8d4747c8785937c31
Gus Salese <gustavo.salese@gmail.com>
2024-01-29 14:43:29 -0300

    primeraentrega2901

--- js/index.js
+++ js/index.js
@@ -1,92 +1,107 @@
 
 // Proyecto de App para Stock e inventario
 
-alert ("Bienvenido! Por favor seleccione la opcion de logueo:")
+// alert ("Bienvenido! Por favor seleccione la opcion de logueo:")
 
 
-let nuevoUsuario = "true"
+// let nuevoUsuario = "true"
 
-const LEGAJOUSUARIO = prompt ("Ingrese su Legajo:") 
-let passwordUsuario = prompt("Ingrese su Password:")
+// const LEGAJOUSUARIO = prompt ("Ingrese su Legajo:") 
+// let passwordUsuario = prompt("Ingrese su Password:")
 
 
-if (LEGAJOUSUARIO != ""){
-    console.log("Ingresando al sistema: " + LEGAJOUSUARIO);
-}
-else {
-    alert("Error! Por favor solicite Usuario del Sistema");
-}
+// if (LEGAJOUSUARIO != ""){
+//     console.log("Ingresando al sistema: " + LEGAJOUSUARIO);
+// }
+// else {
+//     alert("Error! Por favor solicite Usuario del Sistema");
+// }
 
 
 // Buscar Material
 
-let nombreMaterial = prompt ("cual material busca?");
-const PARTCODE = "1234"
+// let nombreMaterial = prompt ("cual material busca?");
+// const PARTCODE = "1234"
 
-if ((nombreMaterial == "refrigerador") || (nombreMaterial == "heladera"))
-    console.log (nombreMaterial + "el codigo de parte buscado es" + "" + PARTCODE);
-    else {
-console.log ("no existe el material")
-    }
+// if ((nombreMaterial == "refrigerador") || (nombreMaterial == "heladera"))
+//     console.log (nombreMaterial + "el codigo de parte buscado es" + "" + PARTCODE);
+//     else {
+// console.log ("no existe el material")
+//     }
 
 
 // Materiales con precio:
 
-let materialLavarropas = 0;
+// let materialLavarropas = 0;
 
-for (let i = 1; i == 10; i++){
+// for (let i = 1; i == 10; i++){
 
-    if (materialLavarropas == 5){
-        console.log ("Se uso el 50% del stock, pedir Compra")
-    }
-}
+//     if (materialLavarropas == 5){
+//         console.log ("Se uso el 50% del stock, pedir Compra")
+//     }
+// }
 
 // Nuevo Usuario
 
-let continuarOno = "si"
+// let continuarOno = "si"
 
-if (continuarOno != true){ 
+// if (continuarOno != true){ 
 
-let nombre = prompt ("Nombre de Usuario:")
-let apellido = prompt ("Apellido de Usuario:")
-const legajo = prompt ("Ingrese su legajo:")
+// let nombre = prompt ("Nombre de Usuario:")
+// let apellido = prompt ("Apellido de Usuario:")
+// const legajo = prompt ("Ingrese su legajo:")
 
-console.log (nombre + " " + apellido + " " + legajo)
-} else {
-    alert ("salir del sistema")
-}
+// console.log (nombre + " " + apellido + " " + legajo)
+// } else {
+//     alert ("salir del sistema")
+// }
 
 
 // Ingreso de entrada de materiales
 
 // let materialLavarropas =0
-let materialHeladera=0
-let materialHorno=0
+// let materialHeladera=0
+// let materialHorno=0
 
-function entradaDeMaterial (materialHeladera, materialLavarropas, materialHorno){
-    return materialHeladera + materialLavarropas + materialHorno
+// let MATERIAL = prompt ("ingresar material")
+
+// while (MATERIAL != "ESC"){
+
+// switch (MATERIAL){
+
+// case "lavarropas": console.log ("Ingreso de lavarropas")
+// break;
+
+// case "horno": console.log("ingreso Horno")
+// break;
+
+// case "heladera": console.log("ingreso heladera")
+// break;
+
+// case "total": console.log (entradaDeMaterial)
+
+// default: console.log ("error de material")
+// }
+// MATERIAL = prompt ("ingresar material")
+// }
+
+// alert ("Saliendo del sisema...")
+
+// Material Valorizado
+
+function valorMaterial (precioMaterial, enviosNacionales){
+
+
+    let ivaDeMaterial = 1.21;
+
+    if (enviosNacionales === true){
+        enviosNacionales = 3000
+    }else enviosNacionales =0
+
+    let resultadoPrecio = (precioMaterial * ivaDeMaterial) + enviosNacionales
+
+    return resultadoPrecio
 }
 
-let MATERIAL = prompt ("ingresar material")
-
-while (MATERIAL != "ESC"){
-
-switch (MATERIAL){
-
-case "lavarropas": console.log ("Ingreso de lavarropas")
-break;
-
-case "horno": console.log("ingreso Horno")
-break;
-
-case "heladera": console.log("ingreso heladera")
-break;
-
-case "total": console.log (entradaDeMaterial)
-
-default: console.log ("error de material")
-}
-MATERIAL = prompt ("ingresar material")
-}
-
-alert ("Saliendo del sisema...")
+let nuevoValor = valorMaterial (3500, false)
+console.log("el nuevo valor es" + nuevoValor)
